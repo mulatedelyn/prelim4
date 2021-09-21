f587db6539fd519832280c0c36a14106b24176dc
Tedelyn Mula <mulatedelyn@gmail.com>
2021-09-21 16:18:54 +0800

    first commit

---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim Project | Profile</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Profile Page</h1>
+    <a href="{{url('/profile')}}">Home</a> | Profile
+</body>
+</html>
